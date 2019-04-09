d9fac6a645ed1c1ea173299c94013f78e0ba99b1
RobertoFigueroa <35494933+RobertoFigueroa@users.noreply.github.com>
2019-04-08 21:45:39 -0600

    Lab3
    commit con todos los cambios

--- Assets/coinBehaviour.cs
+++ Assets/coinBehaviour.cs
@@ -14,7 +14,7 @@ public class coinBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 2, 0);
+        transform.Rotate(0, 100*Time.deltaTime, 0);
      
     }
     private void OnTriggerEnter(Collider other)
